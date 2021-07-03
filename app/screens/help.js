cdfa8bbeaf9d38a7f36bcc0f8aa0e8d002a6a6d3
abrardewa <58102178+abrardewa@users.noreply.github.com>
2021-07-03 13:40:56 +0700

    new

--- app/screens/help.js
+++ app/screens/help.js
@@ -1,240 +1,134 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
+import { useRef } from 'react';
 import {
+  Animated,
   Image,
-  Linking,
-  RefreshControl,
   StyleSheet,
   Text,
   View,
-  ScrollView,
-  TouchableOpacity,
   FlatList,
-  ActivityIndicator,
-  Animated,
-  Alert,
+  ScrollView,
+  Button,
+  Linking,
+  AppRegistry, 
 } from 'react-native';
-import TouchableScale from 'react-native-touchable-scale';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import Spacer from '../components/spacer';
 import {
   Colors,
   H1,
   H2,
   H3,
-  height,
   SPACING,
   SPAN,
   width,
-} from '../../config/ui-config';
+} from './../../config/ui-config';
+const headerImage = require('./../../assets/stethoscope.png');
 
-const axios = require('axios').default;
-
-const categories = [
-  'health',
-  'entertainment',
-  'general',
-  'science',
-  'sports',
-  'technology',
-];
-const itemsPerPage = 3;
-const errorImage = require('./../../assets/error.png');
+import { advices, symptoms } from './../../config/data';
 
 export default function HelpScreen({ navigation }) {
-  const [articles, setArticles] = useState([]);
-  const [page, setPage] = useState(0);
-  const [isRefreshing, setIsRefreshing] = useState(true);
-  const [category, setCategory] = useState(0);
-  const [loading, setLoading] = useState(true);
-  const animateTitle = useRef(new Animated.Value(0)).current;
-  const translateX = animateTitle.interpolate({
-    inputRange: [0, 1],
-    outputRange: [-20, 0],
-  });
-  useEffect(() => {
-    getArticles(page);
-    Animated.timing(animateTitle, {
-      duration: 300,
-      useNativeDriver: true,
-      toValue: 1,
-    }).start();
-  }, [category]);
-
-  function getArticles(selectedPage) {
-    axios
-      .get(
-        `https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSVyqfgX8HUWq_t4kNoG3tSesRgsZRuDe-HoCavfwvIWxL2f9zJTrrtvcPtjgORaih81ZJx-7cjmjX2/pubhtml?gid=292382392&single=true`
-      )
-      .then((res) => {
-        setPage(selectedPage + 1);
-        setArticles(res.data.articles);
-        setLoading(false);
-        setIsRefreshing(false);
-      })
-      .catch(() => {
-        setLoading(false);
-        setIsRefreshing(false);
-        setArticles([]);
-      });
-  }
-
-  function RefreshArticles() {
-    setIsRefreshing(true);
-    getArticles(0);
-  }
-  function changeCategory(index) {
-    if (index != category) {
-      animateTitle.setValue(0);
-      setCategory(index);
-    }
-  }
+  const scrollX = useRef(new Animated.Value(0)).current;
 
   return (
     <View style={styles.container}>
-      <Animated.Text
-        style={{
-          fontSize: H1,
-          textTransform: 'uppercase',
-          fontWeight: 'bold',
-          letterSpacing: 0,
-          marginHorizontal: SPACING,
-          opacity: animateTitle,
-          transform: [{ translateX }],
-        }}
-      >
-        {categories[category]}
-      </Animated.Text>
-      <FlatList
-        data={categories}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        style={{ marginLeft: SPACING * 0.5, height: 80 }}
-        keyExtractor={(item, index) => 'category' + index}
-        renderItem={({ item, index }) => {
-          return (
-            <TouchableScale
-              onPress={() => changeCategory(index)}
-              style={{ height: 42 }}
-            >
-              <View
-                style={[
-                  styles.categoryContainer,
-                  {
-                    backgroundColor:
-                      category === index ? Colors.primary : '#bbb',
-                  },
-                ]}
-              >
-                <Text
-                  style={{
-                    color: 'white',
-                  }}
-                >
-                  {item}
-                </Text>
-              </View>
-            </TouchableScale>
-          );
-        }}
-      />
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        refreshControl={
-          <RefreshControl
-            refreshing={isRefreshing}
-            onRefresh={RefreshArticles}
-          />
-        }
-      >
-        {loading ? (
-          <ActivityIndicator size={'large'} color={Colors.primary} />
-        ) : articles.length === 0 ? (
-          <View
+      <ScrollView>
+        <View style={styles.headerCard}>
+          {/* <Image
+            source={headerImage}
+            style={[styles.headerImage]}
+            resizeMode="contain"
+          /> */}
+          <Text
             style={{
-              height: height * 0.5,
-              alignItems: 'center',
-              justifyContent: 'center',
+              fontWeight: 'bold',
+              color: 'white',
+              fontSize: H2,
+              maxWidth: width * 0.5,
+              marginBottom: SPACING * 0.5,
+              marginLeft: SPACING * 4,
             }}
           >
-            <Image source={errorImage} style={[{ width: 100, height: 100 }]} />
-          </View>
-        ) : (
-          articles.map((article, index) => (
-            <ArticleCard key={'article' + index} article={article} />
-          ))
-        )}
-        <Spacer />
+            Statistik Covid-19 
+            
+          </Text>
+          <Text style={{ color: 'white', maxWidth: width * 0.5 }}>
+          Kota Bandar Lampung dan Lampung selatan
+          </Text>
+        </View>
+
+        <View style={{ padding: SPACING, paddingBottom: 0 }}>
+          <Text style={{ fontWeight: 'bold', fontSize: H2 }}>
+            Grafik 
+          </Text>
+          {/* <Text>
+            Jika mengalami gejala paparan Covid-19 hubungi satgas Covid Kota Bandar Lampung dengan menekan tombol di bawah.
+          </Text>
+          <TouchableOpacity onPress={() => Linking.openURL(`tel:0721252741`)}>
+            <View style={styles.btn}>
+              <Text
+                style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}
+              >
+                Hubungi Sekarang
+              </Text>
+            </View>
+          </TouchableOpacity> */}
+        </View>
+
+
+        
+        
+      
       </ScrollView>
     </View>
   );
 }
 
-function ArticleCard({ article }) {
-  return (
-    <TouchableOpacity onPress={() => Linking.openURL(article.url)}>
-      <View style={styles.articleContainer}>
-        <Image style={styles.articleImg} source={{ uri: article.urlToImage }} />
-        <Text style={styles.articleTime}>
-          {new Date(article.publishedAt).toDateString()}
-        </Text>
-        <View>
-          <Text style={styles.articleTitle} numberOfLines={2}>
-            {article.title}
-          </Text>
-          <Text style={styles.articleSummary} numberOfLines={3}>
-            {article.description}
-          </Text>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
-    marginTop: SPACING * 2,
+    flex: 1,
+    marginTop: 40,
+  },
+  headerCard: {
+    backgroundColor: Colors.primary,
+    elevation: 7,
+    borderRadius: SPACING,
+    marginHorizontal: SPACING,
+    padding: SPACING,
+  },
+  headerImage: {
+    position: 'absolute',
+    width: width * 0.3,
+    right: 20,
+    top: 20,
+    height: 110,
   },
 
-  articleImg: {
-    width: 90,
-    height: 90,
-    borderRadius: SPACING * 0.5,
-    backgroundColor: Colors.lightGrey,
-    marginRight: SPACING * 0.5,
+  adviceContainer: {
+    width: 110,
+    marginRight: SPACING,
   },
-  articleTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    maxWidth: 180,
+  adviceImage: {
+    width: 110,
+    height: 110,
+    marginBottom: SPACING * 0.5,
   },
-  articleTime: {
-    color: Colors.darkGrey,
-    fontSize: 10,
-    position: 'absolute',
-    top: 15,
-    right: SPACING,
-  },
-  articleSummary: {
-    fontSize: 12,
-    maxWidth: width * 0.6,
-    color: Colors.darkGrey,
-  },
-  articleContainer: {
-    marginHorizontal: SPACING * 0.5,
-    marginVertical: SPACING * 0.25,
-    position: 'relative',
-    flexDirection: 'row',
+  symContainer: {
+    width: width - SPACING * 2,
+    marginHorizontal: SPACING,
     backgroundColor: 'white',
-    padding: SPACING * 0.5,
     borderRadius: SPACING,
+    padding: SPACING,
+    marginBottom: SPACING,
+    flexDirection: 'row',
   },
-  categoryContainer: {
-    paddingHorizontal: SPACING,
-    height: 42,
+  btn: {
+    width: width * 0.4,
+    height: 50,
+    borderRadius: 8,
+    marginTop: SPACING,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 20,
-    backgroundColor: Colors.primary,
-    marginVertical: SPACING,
-    marginHorizontal: 5,
+    backgroundColor: 'rgb(0 , 255 ,100)',
   },
 });
